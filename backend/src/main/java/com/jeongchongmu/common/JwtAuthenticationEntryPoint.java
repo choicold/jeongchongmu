094a553a8fa7611f652df87733fad9a26f201bc2
Bangkyunghwan <qkdrudghks0297@naver.com>
2025-11-09 21:53:55 +0900

    feat: 로그인 및 JWT 인증 시스템 구현

---
+++ backend/src/main/java/com/jeongchongmu/common/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.jeongchongmu.common;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+    throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("text/plain;charset=UTF-8");
+        response.getWriter().write("유효하지 않은 토큰(토큰이 없음)");
+    }
+}
--- backend/src/main/java/com/jeongchongmu/user/UserController.java
+++ backend/src/main/java/com/jeongchongmu/user/UserController.java
@@ -1,14 +1,16 @@
 package com.jeongchongmu.user;
 
 
+import com.jeongchongmu.user.dto.LoginRequestDto;
+import com.jeongchongmu.user.dto.LoginResponseDto;
 import com.jeongchongmu.user.dto.SignUpRequestDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+
+@RequestMapping("/api/user/")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
@@ -20,4 +22,17 @@ public class UserController {
 
         return ResponseEntity.ok("회원가입이 완료되었습니다.");
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto){
+        String token = userService.login(loginRequestDto);
+
+        return ResponseEntity.ok(new LoginResponseDto(token));
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok("테스트 성공입니다.");
+    }
+
 }
