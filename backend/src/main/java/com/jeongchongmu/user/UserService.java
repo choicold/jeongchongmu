2a8cfb8269744f971a15d67c80f0020170679e3d
Bangkyunghwan <qkdrudghks0297@naver.com>
2025-11-09 21:53:55 +0900

    feat: 로그인 및 JWT 인증 시스템 구현

--- backend/src/main/java/com/jeongchongmu/user/UserService.java
+++ backend/src/main/java/com/jeongchongmu/user/UserService.java
@@ -1,5 +1,7 @@
 package com.jeongchongmu.user;
 
+import com.jeongchongmu.common.JwtUtil;
+import com.jeongchongmu.user.dto.LoginRequestDto;
 import com.jeongchongmu.user.dto.SignUpRequestDto;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     @Transactional
     public void signUp(SignUpRequestDto signUpRequestDto) {
@@ -28,5 +31,19 @@ public class UserService {
                         .name(signUpRequestDto.getName()).build();
 
         userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public String login(LoginRequestDto loginRequestDto) {
+        User user = userRepository.findByEmail(loginRequestDto.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("등록된 이메일이 없습니다."));
+
+        if(!passwordEncoder.matches(loginRequestDto.getPassword(), user.getPassword())){
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        String token = jwtUtil.createToken(user.getEmail());
+
+        return token;
     }
 }
